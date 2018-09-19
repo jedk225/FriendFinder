31aca356bbf6da9a0adc720c9c7f26e5f15be127
jedi225 <jedtk@uci.edu>
2018-09-19 01:08:05 -0500

    Set up initial routes. Work in progress.

--- server.js
+++ server.js
@@ -3,20 +3,24 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
 
+
 // Tells node that we are creating an "express" server
 var app = express();
 
+
 // Sets an initial port. We"ll use this later in our listener
 var PORT = process.env.PORT || 8080;
+
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// The below points our server to a series of "route" files.
-// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+
+// The below points our server to a series of "route" files which give our server a "map" of how to respond when users visit or request data from various URLs.
+require("./app/routing/htmlRoutes")(app);
+//require("./app/routing/apiRoutes")(app);
+
 
 // =============================================================================
 //Listener: "Starts" the server
