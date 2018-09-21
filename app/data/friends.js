c7b8a148081ef4e06f71c0380cdcc32213a56020
jedi225 <jedtk@uci.edu>
2018-09-21 01:15:47 -0500

    Resolved image spill in modal

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,7 +3,7 @@ var friendsArray = [
     {
         "name": "Emilia",
         "photo": "https://m.media-amazon.com/images/M/MV5BMTk0OTI5MjI3MF5BMl5BanBnXkFtZTcwMTIzMDE0OQ@@._V1_SY1000_CR0,0,666,1000_AL_.jpg",
-        "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+        "scores": [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
     }
 ];
 
